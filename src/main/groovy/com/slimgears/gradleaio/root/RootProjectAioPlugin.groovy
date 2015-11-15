0d8a33334dc68fbd1c0b546bd0a65cf51c9223c3
slim-gears <denis.itskovich@gmail.com>
2015-11-15 13:37:06 +0200

    Adding root project plugin

---
+++ src/main/groovy/com/slimgears/gradleaio/root/RootProjectAioPlugin.groovy
@@ -0,0 +1,32 @@
+package com.slimgears.gradleaio.root
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Delete;
+
+class RootProjectAioPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.apply plugin: "idea"
+
+        project.tasks.ideaProject.doLast {
+            copy { from '.' into '.idea/' include '*.ipr' rename { "modules.xml" } }
+            copy { from '.' into '.idea/' include '*.iws' rename { "workspace.xml" } }
+            project.delete "${project.name}.ipr"
+            project.delete "${project.name}.iws"
+        }
+
+        project.allprojects {
+            it.apply plugin: "idea"
+
+            it.repositories {
+                jcenter()
+                maven { url "https://jitpack.io" }
+            }
+        }
+
+        project.tasks.create(name: 'clean', type: Delete) {
+            delete project.rootProject.buildDir
+        }
+    }
+}
