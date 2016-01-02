8e0514e7ca5fa30361701f0716ec1632de7faf5b
slim-gears <denis.itskovich@gmail.com>
2016-01-02 08:28:04 +0200

    Idea workspace adjustments Version 0.4.9

--- src/main/groovy/com/slimgears/gradleaio/root/RootProjectAioPlugin.groovy
+++ src/main/groovy/com/slimgears/gradleaio/root/RootProjectAioPlugin.groovy
@@ -1,5 +1,6 @@
 package com.slimgears.gradleaio.root
 import com.slimgears.gradleaio.internal.ConfigContainer
+import groovy.xml.XmlUtil
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -16,10 +17,52 @@ class RootProjectAioPlugin implements Plugin<Project> {
             }
         }
 
+        project.idea.workspace.iws.withXml { provider ->
+            def runManagerNode = provider.asNode().find { it.@name == 'RunManager'}
+            def defaultJunitConfig = runManagerNode.find { it.@type == 'JUnit' && it.@default }
+            defaultJunitConfig.find { it.@name == 'TEST_OBJECT' }.@value = "package"
+            defaultJunitConfig.find { it.@name == 'WORKING_DIRECTORY' }.@value = '$MODULE_DIR$'
+            defaultJunitConfig.find { it.@name == 'VM_PARAMETERS'}.@value = '-ea'
+            defaultJunitConfig.find { it.@name == 'PASS_PARENT_ENVS'}.@value = true
+            defaultJunitConfig.find { it.@name == 'TEST_SEARCH_SCOPE' }.value.@defaultName = 'singleModule'
+
+            project.allprojects {
+                def configNode = runManagerNode.appendNode('configuration', [default: false, name: "$it.name test", type: 'JUnit', factoryName: 'JUnit'])
+                configNode.appendNode('module', [name: it.name])
+                configNode.appendNode('option', [name: 'TEST_OBJECT', value: 'package'])
+                configNode.appendNode('option', [name: 'VM_PARAMETERS', value: '-ea'])
+                configNode.appendNode('option', [name: 'WORKING_DIRECTORY', value: '$MODULE_DIR$'])
+                configNode.appendNode('option', [name: 'PASS_PARENT_ENVS', value: true])
+                configNode.appendNode('option', [name: 'TEST_SEARCH_SCOPE'])
+                        .appendNode('value', [defaultName: 'singleModule'])
+            }
+        }
+
         project.tasks.ideaProject.doLast {
-            copy { from '.' into '.idea/' include '*.ipr' rename { "modules.xml" } }
-            copy { from '.' into '.idea/' include '*.iws' rename { "workspace.xml" } }
+            def rootNode = new Node(null, 'project', [version: 4])
+            def modulesNode = rootNode
+                    .appendNode('component', [name: 'ProjectModuleManager'])
+                    .appendNode('modules')
+
+            def rootUri = project.projectDir.toURI()
+            project.allprojects {
+                def projectUri = it.projectDir.toURI()
+                def relativeDir = projectUri.toString().substring(rootUri.toString().length())
+                modulesNode.appendNode('module', [
+                        fileurl: "file://\$PROJECT_DIR\$/${relativeDir}${it.name}.iml",
+                        filepath: "\$PROJECT_DIR\$/${relativeDir}${it.name}.iml"])
+            }
+
+            project.file('.idea').mkdir()
+            def filePrinter = new PrintWriter("$project.projectDir/.idea/modules.xml")
+            def xmlPrinter = new XmlNodePrinter(filePrinter)
+            filePrinter.println('<?xml version="1.0" encoding="UTF-8"?>')
+            xmlPrinter.print(rootNode)
             project.delete "${project.name}.ipr"
+        }
+
+        project.tasks.ideaWorkspace.doLast {
+            project.copy { from '.' into '.idea/' include "${project.name}.iws" rename { "workspace.xml" } }
             project.delete "${project.name}.iws"
         }
     }
